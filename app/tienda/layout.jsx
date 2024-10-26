ea591410bd17d60455f5cff7435d90e9630877f5
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-10-25 23:25:39 -0600

    Schema

--- app/tienda/layout.jsx
+++ app/tienda/layout.jsx
@@ -1,6 +1,9 @@
+
+
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import Head from "next/head";
+
 
 export const metadata = {
   title: "Recetas Saludables: 10 Platos Fáciles y Rápidos para | Web 2024",
@@ -12,11 +15,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+       
+        <test/>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
       <body className={inter.className}>{children}</body>
       <link rel="canonical" href={metadata.canonical} />
+ 
     </html>
   );
 }

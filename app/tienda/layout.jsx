645a17cc26068b1188c4efa1521c4d755baee67b
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-10-27 09:57:37 -0600

    Refactor metadata in layout.jsx and update Open Graph image URL

--- app/tienda/layout.jsx
+++ app/tienda/layout.jsx
@@ -7,13 +7,13 @@ export const metadata = {
   title: "Recetas Saludables: 10 Platos Fáciles y Rápidos para | Web 2024",
   description: "Descubre 12 recetas saludables, fáciles y ddddd ssss para ddddssssssd en la cociffffffna. ¡Comienza tu viaje culinario hoydd!",
   canonical: "https://webseo-ten.vercel.app/tienda",
-  
+
   openGraph: {
     title: 'Título para Redes Sociales',
     description: 'Descripción para redes sociales',
     type: 'website',
     url: 'https://tusitio.com',
-    image: 'https://tusitio.com/og-image.jpg',
+    image: 'https://www.nerdwallet.com/front-page-images/_next/image?url=/nw_plus_assets/nwplus-hero-image-v3.png&w=1920&q=100',
     locale: 'es_ES',
   },
 

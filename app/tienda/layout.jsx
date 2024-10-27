0b7869720395a5c896aca9452d61a54fb10b1f2c
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-10-27 09:31:56 -0600

    Refactor metadata in layout.jsx and add Open Graph tags

--- app/tienda/layout.jsx
+++ app/tienda/layout.jsx
@@ -5,10 +5,20 @@ import Head from "next/head";
 
 export const metadata = {
   title: "Recetas Saludables: 10 Platos Fáciles y Rápidos para | Web 2024",
-  description:
-    "Descubre 12 recetas saludables, fáciles y ddddd ssss para ddddssssssd en la cociffffffna. ¡Comienza tu viaje culinario hoydd!",
+  description: "Descubre 12 recetas saludables, fáciles y ddddd ssss para ddddssssssd en la cociffffffna. ¡Comienza tu viaje culinario hoydd!",
   canonical: "https://webseo-ten.vercel.app/tienda",
+  
+   // Open Graph - para compartir en redes sociales
+   openGraph: {
+    title: 'Título para Redes Sociales',
+    description: 'Descripción para redes sociales',
+    type: 'website',
+    url: 'https://tusitio.com',
+    image: 'https://tusitio.com/og-image.jpg',
+    locale: 'es_ES',
+  },
 };
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
